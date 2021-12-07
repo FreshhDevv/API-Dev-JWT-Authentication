6ffa14a421fc069e1861cdc59e723085cb608ce4
FreshDev <billyhans90@gmail.com>
2021-12-07 19:51:41 +0000

    Add API Controller settings Create API routes and middleware

---
+++ app/Http/Controllers/Api/CourseController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Course;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    // COURSE ENROLLMENT API - POST
+    public function courseEnrollment(Request $request) {
+
+    }
+
+    // TOTAL COURSE ENROLLMENT API - GET
+    public function totalCourses() {
+
+    }
+
+    // DELETE COURSE API - GET
+    public function deleteCourse($id) {
+
+    }
+
+
+}
