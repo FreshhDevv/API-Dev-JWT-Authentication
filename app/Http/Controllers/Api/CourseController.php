93f9e370ebb374be4a32b09af984ae420e3153ef
FreshDev <billyhans90@gmail.com>
2021-12-09 00:50:12 +0000

    Add the user course enrollment API

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -10,6 +10,29 @@ class CourseController extends Controller
 {
     // COURSE ENROLLMENT API - POST
     public function courseEnrollment(Request $request) {
+        // VALIDATION
+        $request->validate([
+            'title'=>'required',
+            'description'=>'required',
+            'total_videos'=>'required'
+        ]);
+
+        // CREATE COURSE OBJECT
+        $course = new Course();
+
+        $course->user_id = auth()->user()->id;
+        $course->title = $request->title;
+        $course->description = $request->description;
+        $course->total_videos = $request->total_videos;
+        
+        $course->save();
+
+        // SEND RESPONSE
+        return response()->json([
+            'status' => 1,
+            'message' => 'Course created successfully'
+        ]);
+
 
     }
 
